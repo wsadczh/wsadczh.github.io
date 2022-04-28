e4d43dbc2a73225cd962f40716ed4a443874ab4a
wsadczh <654032921@qq.com>
2022-04-28 18:55:49 +0800

    first

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,9 @@ if [ -z "$GITHUB_TOKEN" ]; then
 else
   msg='来自github actions的自动部署'
   githubUrl=https://wsadczh:${GITHUB_TOKEN}@github.com/wsadczh/wsadczh.github.io.git
-  git config user.name "wsadczh"
+  git config --global user.name "wsadczh"
   git config --global init.defaultBranch main
-  git config user.email "654032921@qq.com"
+  git config --global user.email "654032921@qq.com"
 fi
 git init
 git add -A
